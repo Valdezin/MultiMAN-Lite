d1fdc5ee71948f5ddf005a4ac421a82ea372c7eb
VLDZ <ops@vldz.ga>
2022-11-08 13:17:46 +0800

    added jail command

---
+++ commands/actions/jail.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'pls jail',
+    code:
+    `
+    $description[
+        <@$authorID> jailed <@$mentioned[1]>! :oncoming_police_car: 
+            $image[1;https://some-random-api.ml/canvas/overlay/jail?avatar=$replaceText[$userAvatar[$mentioned[1]];webp;png];jailed.png]
+    ]
+        $onlyIf[$mentioned[1]!=$authorID;You need to mention someone to jail, also you obviously can't jail yourself!]
+        
+        
+    `
+}
